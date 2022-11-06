24d8a2225a2b1d04f15fc76671140bcd8d833aec
Grigory <noellhill@yandex.ru>
2022-11-06 08:48:57 +0300

    простая база данных фильма на JS

--- script.js
+++ script.js
@@ -33,29 +33,52 @@
 // alert(`Какое великое имя! ${arr[1]}`);
 // confirm(`Могу ли я вас, ${arr[0]} ${arr[1]} называть своим господином`);
 
-let incr=10,
-	decr=10;
+// let incr=10,
+// 	decr=10;
 
-incr++;
-decr--;
+// incr++;
+// decr--;
 
-console.log(incr);
-console.log(decr);
+// console.log(incr);
+// console.log(decr);
 
 // ++incr;
 // --decr;
 
-console.log(incr++);
-console.log(decr--  );
+// console.log(incr++);
+// console.log(decr--  );
 
-// || или   && и
+// || или   && и   тру и(&&) фалс =фалс оператор и возвращает тру если все тру, но стоит быть одному фалс и итог фалс. или (||)
 
-const isChecked=true,
-	  isClose= false;
+// const isChecked=true,
+// 	  isClose= false;
 
-console.log(isChecked || isClose);
+// console.log(isChecked || isClose);
 
-console.log(isChecked && isClose);
+// console.log(isChecked && isClose);
+
+// const m=true,
+// 	  f=false;
+
+// console.log(m&&f);
+
+const  numberOfFilms= +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB={
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+const a=prompt('Один из последних просмотренных фильмов?',''),
+	  b=prompt('На сколько оцените его?', ''),
+	  c=prompt('Один из последних просмотренных фильмов?',''),
+	  d=prompt('На сколько оцените его?', '');
 
 
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
 
+console.log(personalMovieDB);
