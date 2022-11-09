d4b32cce386b603e4e034c967845855da3ff7dbd
Grigory <noellhill@yandex.ru>
2022-11-10 00:37:58 +0300

    решение второй задачи

--- script.js
+++ script.js
@@ -85,23 +85,134 @@
 
 // повтор решения задачи
 
-const numberOfFilms= +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms= +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB={
+// const personalMovieDB={
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false,
+// };
+
+// const a=prompt('Один из последних просмотренных фильмов?', '');
+// const b=prompt('На сколько оцените его?', '');
+// const c=prompt('Один из последних просмотренных фильмов?', '');
+// const d=prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a]=b;
+// personalMovieDB.movies[c]=d;
+
+// console.log(personalMovieDB);
+// с alert не получается, забыл + в 88 строке и правильное написание prompt
+
+// использование условий урок 13; условие в if должно быть приведено в булиновый тип
+// if (4==9){
+// 	console.log('ok!');
+// } else{
+// 	console.log('Error');
+// }
+
+// const num=50;
+
+// if (num<49){
+// 	console.log('error');
+// } else if (num>100) {
+// 	console.log('Много');
+// } else {
+// 	console.log('ok!')
+// }
+
+// тернарный оператор позволяет экономить место кода. есть три аргумента , поэтому тернарный оператор.
+
+// (num===50) ? console.log('ok'): console.log('error');
+
+// свитч помогает решать проблему связанную с большим количеством ветвлений. всегда идет на строгое сравнение!!! === можно работать с числами и строками
+// const num=51;
+
+// switch(num){
+// 	case 49:
+// 		console.log('error');
+// 		break;
+// 	case 100:
+// 		console.log ('error');
+// 		break;
+// 	case 50:
+// 		console.log('ok');
+// 		break;
+// 	default:
+// 		console.log('not yet');
+// 		break;
+// }
+
+// циклы урок 14 для повторения однотипных действий
+
+// let num=50;
+
+// while(num<=55){
+// 	console.log(num);
+// 	num++;
+// }
+
+// заставляем цикл что=то сделать, потом проверит условия и выйти
+// do{
+// 	console.log(num);
+// 	num++;
+// }
+// while(num<55);
+
+// for (let i=1; i<8; i++){
+// 	if(i===6){
+// 		// break;
+// 		continue;
+// 	}
+// 	console.log(i);
+// }
+
+
+// практика
+
+const numberOfFilms= +prompt('Сколько фильмов смотрели?', '');
+
+let personalMovieDB={
 	count: numberOfFilms,
 	movies: {},
 	actors: {},
-	genres: [],
+	genres:[],
 	privat: false,
 };
 
-const a=prompt('Один из последних просмотренных фильмов?', '');
-const b=prompt('На сколько оцените его?', '');
-const c=prompt('Один из последних просмотренных фильмов?', '');
-const d=prompt('На сколько оцените его?', '');
+// const a= prompt('Один из последних просмотренных фильмов?', '');
+// const b= prompt('На сколько оцените его?', '');
+// const c= prompt('Один из последних просмотренных фильмов?', '');
+// const d= prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+// personalMovieDB.movies[a]=b;
+// personalMovieDB.movies[c]=d;
+
+
+for (let i=0; i<3; i++){
+	let a= prompt('Один из последних просмотренных фильмов?', '');
+	let b= prompt('На сколько оцените его?', '');
+
+	if(a!=null && b!=null && a!='' && b!='' && a.length<50){
+		personalMovieDB.movies[a]=b;
+		console.log("done");
+	} else{
+		console.log('error');
+		i--; 
+	}
+	
+}
+
+if (personalMovieDB.count<10){
+	alert('Просмотрено довольно мало фильмов');
+} else if(personalMovieDB.count>10 && personalMovieDB.count<30){
+	alert('Вы классический зритель');
+} else if(personalMovieDB.count>30){
+	alert('Вы киноман!');
+} else{
+	alert('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
-// с alert не получается, забыл + в 88 строке и правильное написание prompt
