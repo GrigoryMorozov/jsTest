38a7fc9738f132af7b46c88a5a199c13b4725f97
Grigory <noellhill@yandex.ru>
2022-12-06 21:51:13 +0300

    Повторение. Массивы.

--- script.js
+++ script.js
@@ -731,73 +731,109 @@
 
 
 
-const personalMovieDB={
-	count: 0,
-	movies:{},
-	actors:{},
-	genres:[],
-	privat:false,
-	start: function(){
-		personalMovieDB.count= +prompt('Сколько фильмов вы уже посмотрели','');
+// const personalMovieDB={
+// 	count: 0,
+// 	movies:{},
+// 	actors:{},
+// 	genres:[],
+// 	privat:false,
+// 	start: function(){
+// 		personalMovieDB.count= +prompt('Сколько фильмов вы уже посмотрели','');
 
-		while(personalMovieDB.count==''|| personalMovieDB.count==null|| isNaN(personalMovieDB.count)){
-			personalMovieDB.count=+prompt('Сколько фильмов вы уже посмотрели','');
-		}
-	},
-	rememberMyFilms:function(){
-		for (let i=0; i<2; i++){
-			const a=prompt('Один из последних просмотренных фильмов?', ''),
-			  	b=prompt('На сколько оцените его?','');
-				if (a!=null&&b!=null&&a!=''&&b!=''&&a.length<50){
-				personalMovieDB.movies[a]=b;
-				console.log('done');		
-				} else{
-					console.log('error');
-					i--;
-				}
-		}
-	},
-	detectPersonalLevel: function(){
-		if (personalMovieDB.count<10){
-			console.log('Просмотрено довольно много фильмов');
-		} else if(personalMovieDB.count>=10&&personalMovieDB.count<30){
-			console.log('Вы классический зритель');
-		} else if (personalMovieDB.count>=30){
-			console.log('Вы киноман');
-		} else{
-			console.log('Произошла ошибка');
-		}
-	},
-	showMyDB:function(hidden){
-		if(!hidden){
-			console.log(personalMovieDB);
-		}
-	},
-	toggleVisibleMyDB:function(){
-		if(personalMovieDB.privat){
-			personalMovieDB.privat=false;
-		}else{
-			personalMovieDB.privat=true;
-		}
-	},
-	writeYourGenres: function(){
-		for(let i=1; i<=3; i++){
-			let genre=prompt(`Ваш любимый жанр под номером ${i}`);
-			if(genre==''||genre==null){
-				console.log('Вы ввели неккоретные данные или не ввели вообще');
-				i--;
-			}else{
-				personalMovieDB.genres[i-1]=genre;
-			}
+// 		while(personalMovieDB.count==''|| personalMovieDB.count==null|| isNaN(personalMovieDB.count)){
+// 			personalMovieDB.count=+prompt('Сколько фильмов вы уже посмотрели','');
+// 		}
+// 	},
+// 	rememberMyFilms:function(){
+// 		for (let i=0; i<2; i++){
+// 			const a=prompt('Один из последних просмотренных фильмов?', ''),
+// 			  	b=prompt('На сколько оцените его?','');
+// 				if (a!=null&&b!=null&&a!=''&&b!=''&&a.length<50){
+// 				personalMovieDB.movies[a]=b;
+// 				console.log('done');		
+// 				} else{
+// 					console.log('error');
+// 					i--;
+// 				}
+// 		}
+// 	},
+// 	detectPersonalLevel: function(){
+// 		if (personalMovieDB.count<10){
+// 			console.log('Просмотрено довольно много фильмов');
+// 		} else if(personalMovieDB.count>=10&&personalMovieDB.count<30){
+// 			console.log('Вы классический зритель');
+// 		} else if (personalMovieDB.count>=30){
+// 			console.log('Вы киноман');
+// 		} else{
+// 			console.log('Произошла ошибка');
+// 		}
+// 	},
+// 	showMyDB:function(hidden){
+// 		if(!hidden){
+// 			console.log(personalMovieDB);
+// 		}
+// 	},
+// 	toggleVisibleMyDB:function(){
+// 		if(personalMovieDB.privat){
+// 			personalMovieDB.privat=false;
+// 		}else{
+// 			personalMovieDB.privat=true;
+// 		}
+// 	},
+// 	writeYourGenres: function(){
+// 		for(let i=1; i<=3; i++){
+// 			let genre=prompt(`Ваш любимый жанр под номером ${i}`);
+// 			if(genre==''||genre==null){
+// 				console.log('Вы ввели неккоретные данные или не ввели вообще');
+// 				i--;
+// 			}else{
+// 				personalMovieDB.genres[i-1]=genre;
+// 			}
 			
 
-		}
-		personalMovieDB.genres.forEach((item,i)=>{
-			console.log(`Любимый жанр ${i+1} - это ${item}`);
-		})
-	}
+// 		}
+// 		personalMovieDB.genres.forEach((item,i)=>{
+// 			console.log(`Любимый жанр ${i+1} - это ${item}`);
+// 		})
+// 	}
 
-};
+// };
 
 
 
+
+// повторение. работа с массивами
+
+// let count=+prompt('Сколько значений необходимо ввести?','');
+// console.log(`будет введено ${count} значений в массив`);
+// let arr=[];
+
+// for (let i=0; i<count; i++){
+// 	let answer=prompt(`Введите ${i+1} значение в массив`);
+// 	arr.push(answer);
+// };
+
+// console.log(arr);
+
+// console.log('Сейчас поиграем');
+
+// function startRand(){
+// 	for(let i=0; i<arr.length; i++){
+// 		let change=Math.floor(Math.random()*arr.length);
+// 		console.log(arr[change]);
+// 	}
+	
+// }
+// startRand();
+
+// генератор случайных дразнилок
+const randFirstArr=['глаз', 'рука', 'жопа'];
+const randSecondArr=['древняя', 'лишняя', 'нехорошая'];
+const randThirdArr=['кастрюля','пачка','редиска','жвачка'];
+
+const randFA=randFirstArr[Math.floor(Math.random()*randFirstArr.length)];
+const randSA=randSecondArr[Math.floor(Math.random()*randSecondArr.length)];
+const randTA=randThirdArr[Math.floor(Math.random()*randThirdArr.length)];
+
+let randResult=`У тебя ${randFA} словно ${randSA} ${randTA}!`;
+console.log(randResult);
